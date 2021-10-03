bb02387bea542764cbdff34aa5af1bc7999d946f
maulanaaghnii <maulanaaghni.contact@gmail.com>
2021-10-03 17:16:51 +0700

    praktikum 1 dan 2

---
+++ 2DasproPraktikum/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Kamus
+    int umur; //deklarasi
+    int u; //deklarasi
+    float tinggi; //deklarasi
+    char ukuranBaju = 'L';//
+
+    //algoritma
+    umur = 12;
+    u = umur;
+    tinggi = 168;
+
+    cout << "Hallo aku adalah programmer." << endl;
+    cout << "Umurku " << u;
+    cout << endl;
+    cout << "Tinggiku " << tinggi;
+    cout << "\n";
+    cout << "Ukuran bajuku " << ukuranBaju;
+
+    return 0;
+}
